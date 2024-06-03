e26f2a9edfb87e2d64068e3b7c476c1e15e92b79
おりょう <oryo.developer@gmail.com>
2024-06-04 03:22:20 +0900

    Appを作成

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_qiita_app/pages/articles_page/articles_page.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ArticlesPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
