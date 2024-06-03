a6ff847774e11f05b7a90aaded805d29cf052f88
おりょう <oryo.developer@gmail.com>
2024-06-04 03:24:08 +0900

    ArticleRepositoryを作成

---
+++ lib/repositories/article_repository/article_repository.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_qiita_app/models/article/article.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'article_repository.g.dart';
+
+@RestApi()
+abstract class ArticleRepository {
+  factory ArticleRepository(Dio dio, {String? baseUrl}) = _ArticleRepository;
+
+  @GET('/items')
+  Future<List<Article>> fetchArticles({
+    @Query('page') required int page,
+    @Query('query') String? query,
+  });
+}
