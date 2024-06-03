fe9f54bdf8ae44c20d0726c68b6ca76c326da9b8
おりょう <oryo.developer@gmail.com>
2024-06-04 03:24:29 +0900

    ArticlesPageStateを作成

---
+++ lib/pages/articles_page/articles_page_state/articles_page_state.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_qiita_app/models/article/article.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'articles_page_state.freezed.dart';
+
+@freezed
+class ArticlesPageState with _$ArticlesPageState {
+  const factory ArticlesPageState({
+    @Default(1) int page,
+    List<Article>? articles,
+  }) = _ArticlesPageState;
+}
