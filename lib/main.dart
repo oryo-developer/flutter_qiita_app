6d28cf4f6c97486c05d9f2515550fa7767340289
おりょう <oryo.developer@gmail.com>
2024-06-04 20:09:20 +0900

    ディレクトリを変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_qiita_app/app.dart';
 import 'package:flutter_qiita_app/shared_preferences.dart';
+import 'package:flutter_qiita_app/ui/app.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
