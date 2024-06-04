2eedc6697e04fa87b6dc4d7229bd483702950224
おりょう <oryo.developer@gmail.com>
2024-06-04 18:54:49 +0900

    transparentとgreen60を追加

--- lib/ui/theme/theme_color/theme_color.dart
+++ lib/ui/theme/theme_color/theme_color.dart
@@ -3,6 +3,7 @@ import 'package:flutter_qiita_app/ui/theme/theme_color/dark_theme_color.dart';
 import 'package:flutter_qiita_app/ui/theme/theme_color/light_theme_color.dart';
 
 abstract class ThemeColor {
+  static const transparent = Color(0x00000000);
   static const white = Color(0xffffffff);
   static const gray10 = Color(0xfff5f6f6);
   static const gray20 = Color(0xffedeeee);
@@ -10,6 +11,7 @@ abstract class ThemeColor {
   static const gray100 = Color(0xff2f3232);
   static const gray110 = Color(0xff1d2020);
   static const black = Color(0xff000000);
+  static const green60 = Color(0xff67cb1b);
 
   Color get background;
   Color get surface;
