89690c37d275886f5acc26632aa7867c7f6c2918
おりょう <oryo.developer@gmail.com>
2024-06-04 18:55:56 +0900

    paddingを作成

--- lib/extensions/build_context_extension.dart
+++ lib/extensions/build_context_extension.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_qiita_app/ui/theme/theme_color/theme_color.dart';
 
 extension BuildContextExtension on BuildContext {
+  EdgeInsets get padding {
+    return MediaQuery.paddingOf(this);
+  }
+
   ThemeColor get themeColor {
     final brightness = Theme.of(this).brightness;
     return ThemeColor.brightness(brightness: brightness);
