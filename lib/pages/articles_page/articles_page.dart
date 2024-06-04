3a63e1f3923b7b69603aab77f6608ff90ce28032
おりょう <oryo.developer@gmail.com>
2024-06-04 14:23:32 +0900

    記事一覧のUIを作成

--- lib/pages/articles_page/articles_page.dart
+++ lib/pages/articles_page/articles_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_qiita_app/pages/articles_page/articles_page_provider.dart';
+import 'package:flutter_qiita_app/pages/articles_page/widgets/articles_page_article_footer.dart';
+import 'package:flutter_qiita_app/pages/articles_page/widgets/articles_page_article_header.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ArticlesPage extends StatelessWidget {
@@ -7,23 +9,43 @@ class ArticlesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Consumer(builder: (context, ref, child) {
-        final articles = ref.watch(articlesPageProvider.select((state) {
-          return state.articles;
-        }));
+    return Scaffold(body: Consumer(builder: (context, ref, child) {
+      final articles = ref.watch(articlesPageProvider.select((state) {
+        return state.articles;
+      }));
 
-        if (articles == null) {
-          return const Center(child: CircularProgressIndicator());
-        }
+      if (articles == null) {
+        return const Center(child: CircularProgressIndicator());
+      }
 
-        return ListView.builder(
-          itemCount: articles.length,
-          itemBuilder: (context, index) {
-            return Text(articles[index].title);
-          },
-        );
-      }),
-    );
+      return ListView.separated(
+        itemCount: articles.length,
+        itemBuilder: (context, index) {
+          final article = articles[index];
+          return Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ArticlesPageArticleHeader(article: article),
+                const SizedBox(height: 8),
+                Text(
+                  article.title,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                ArticlesPageArticleFooter(article: article),
+              ],
+            ),
+          );
+        },
+        separatorBuilder: (context, index) {
+          return const SizedBox(height: 16);
+        },
+      );
+    }));
   }
 }
