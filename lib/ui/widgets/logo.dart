b2c0beb583e6f46de4ac947f72f43fd361696c94
おりょう <oryo.developer@gmail.com>
2024-06-04 18:55:36 +0900

    Logoを作成

---
+++ lib/ui/widgets/logo.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_qiita_app/extensions/color_extension.dart';
+import 'package:flutter_qiita_app/gen/assets.gen.dart';
+import 'package:flutter_qiita_app/ui/theme/theme_color/theme_color.dart';
+
+class Logo extends StatelessWidget {
+  const Logo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 10),
+      decoration: BoxDecoration(
+        color: ThemeColor.green60,
+        borderRadius: BorderRadius.circular(4),
+      ),
+      child: Assets.svgs.logo.svg(
+        height: 18,
+        colorFilter: ThemeColor.white.filter,
+      ),
+    );
+  }
+}
