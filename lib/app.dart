2ba68bd17c9305c3695fa48e830922e22cc691ea
おりょう <oryo.developer@gmail.com>
2024-06-04 20:06:43 +0900

    themeModeを設定

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_qiita_app/ui/pages/articles_page/articles_page.dart';
 import 'package:flutter_qiita_app/ui/theme/theme_data_with_theme_color.dart';
+import 'package:flutter_qiita_app/ui/theme/theme_mode_provider.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class App extends StatelessWidget {
+class App extends ConsumerWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       home: const ArticlesPage(),
       theme: ThemeDataWithThemeColor.light,
       darkTheme: ThemeDataWithThemeColor.dark,
+      themeMode: ref.watch(themeModeProvider),
       debugShowCheckedModeBanner: false,
     );
   }
