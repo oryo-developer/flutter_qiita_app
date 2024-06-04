46486662cdeb45a7e937d6db3c39e0e1fff68943
おりょう <oryo.developer@gmail.com>
2024-06-04 20:03:00 +0900

    sharedPreferencesを初期化

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_qiita_app/app.dart';
+import 'package:flutter_qiita_app/shared_preferences.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  sharedPreferences = await SharedPreferences.getInstance();
+
   runApp(const ProviderScope(child: App()));
 }
