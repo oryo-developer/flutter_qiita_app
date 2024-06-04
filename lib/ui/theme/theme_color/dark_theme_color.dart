8d160d9d8193b8f21bc34fa4a7d532d3fe8ef1e9
おりょう <oryo.developer@gmail.com>
2024-06-04 17:36:02 +0900

    DarkThemeColorを作成

---
+++ lib/ui/theme/theme_color/dark_theme_color.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_qiita_app/ui/theme/theme_color/theme_color.dart';
+
+class DarkThemeColor extends ThemeColor {
+  @override
+  Color get background {
+    return ThemeColor.gray110;
+  }
+
+  @override
+  Color get surface {
+    return ThemeColor.gray100;
+  }
+
+  @override
+  Color get surfaceVariant {
+    return ThemeColor.gray80;
+  }
+
+  @override
+  Color get highEmphasis {
+    return ThemeColor.white.withOpacity(0.92);
+  }
+
+  @override
+  Color get mediumEmphasis {
+    return ThemeColor.white.withOpacity(0.65);
+  }
+}
