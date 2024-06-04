81aba0afb0214816dfd9cef6f718d1fe4f9a22a0
おりょう <oryo.developer@gmail.com>
2024-06-04 18:56:20 +0900

    AppBarを表示

--- lib/ui/pages/articles_page/articles_page.dart
+++ lib/ui/pages/articles_page/articles_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_qiita_app/ui/pages/articles_page/articles_page_provider.
 import 'package:flutter_qiita_app/ui/pages/articles_page/widgets/articles_page_article_footer.dart';
 import 'package:flutter_qiita_app/ui/pages/articles_page/widgets/articles_page_article_header.dart';
 import 'package:flutter_qiita_app/ui/widgets/launch_url.dart';
+import 'package:flutter_qiita_app/ui/widgets/logo.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ArticlesPage extends StatelessWidget {
@@ -11,47 +12,51 @@ class ArticlesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Consumer(builder: (context, ref, child) {
-      final articles = ref.watch(articlesPageProvider.select((state) {
-        return state.articles;
-      }));
+    return Scaffold(
+      appBar: AppBar(title: const Logo()),
+      body: Consumer(builder: (context, ref, child) {
+        final articles = ref.watch(articlesPageProvider.select((state) {
+          return state.articles;
+        }));
 
-      if (articles == null) {
-        return const Center(child: CircularProgressIndicator());
-      }
+        if (articles == null) {
+          return const Center(child: CircularProgressIndicator());
+        }
 
-      return ListView.separated(
-        itemCount: articles.length,
-        itemBuilder: (_, index) {
-          final article = articles[index];
-          return LaunchUrl(
-            article.url,
-            child: Container(
-              padding: const EdgeInsets.all(16),
-              color: context.themeColor.surface,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ArticlesPageArticleHeader(article: article),
-                  const SizedBox(height: 8),
-                  Text(
-                    article.title,
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+        return ListView.separated(
+          padding: EdgeInsets.only(top: 24, bottom: context.padding.bottom),
+          itemCount: articles.length,
+          itemBuilder: (_, index) {
+            final article = articles[index];
+            return LaunchUrl(
+              article.url,
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                color: context.themeColor.surface,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ArticlesPageArticleHeader(article: article),
+                    const SizedBox(height: 8),
+                    Text(
+                      article.title,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 8),
-                  ArticlesPageArticleFooter(article: article),
-                ],
+                    const SizedBox(height: 8),
+                    ArticlesPageArticleFooter(article: article),
+                  ],
+                ),
               ),
-            ),
-          );
-        },
-        separatorBuilder: (context, index) {
-          return const SizedBox(height: 16);
-        },
-      );
-    }));
+            );
+          },
+          separatorBuilder: (context, index) {
+            return const SizedBox(height: 16);
+          },
+        );
+      }),
+    );
   }
 }
