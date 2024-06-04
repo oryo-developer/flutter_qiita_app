c40c96107a316cb975dda3506194d8d9ad0eddcc
おりょう <oryo.developer@gmail.com>
2024-06-04 17:35:48 +0900

    LightThemeColorを作成

---
+++ lib/ui/theme/theme_color/light_theme_color.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_qiita_app/ui/theme/theme_color/theme_color.dart';
+
+class LightThemeColor extends ThemeColor {
+  @override
+  Color get background {
+    return ThemeColor.gray10;
+  }
+
+  @override
+  Color get surface {
+    return ThemeColor.white;
+  }
+
+  @override
+  Color get surfaceVariant {
+    return ThemeColor.gray20;
+  }
+
+  @override
+  Color get highEmphasis {
+    return ThemeColor.black.withOpacity(0.87);
+  }
+
+  @override
+  Color get mediumEmphasis {
+    return ThemeColor.black.withOpacity(0.6);
+  }
+}
