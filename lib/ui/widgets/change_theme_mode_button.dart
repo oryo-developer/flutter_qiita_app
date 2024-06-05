8851bedf0f26c7b68a59468660500781a42e214b
おりょう <oryo.developer@gmail.com>
2024-06-06 00:46:01 +0900

    範囲を縮小

--- lib/ui/widgets/change_theme_mode_button.dart
+++ lib/ui/widgets/change_theme_mode_button.dart
@@ -4,18 +4,20 @@ import 'package:flutter_qiita_app/extensions/theme_mode_extension.dart';
 import 'package:flutter_qiita_app/ui/theme/theme_mode_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class ChangeThemeModeButton extends ConsumerWidget {
+class ChangeThemeModeButton extends StatelessWidget {
   const ChangeThemeModeButton({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final themeMode = ref.watch(themeModeProvider);
+  Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(right: 16),
-      child: GestureDetector(
-        onTap: ref.read(themeModeProvider.notifier).change,
-        child: Icon(themeMode.icon, color: context.themeColor.mediumEmphasis),
-      ),
+      child: Consumer(builder: (context, ref, child) {
+        final themeMode = ref.watch(themeModeProvider);
+        return GestureDetector(
+          onTap: ref.read(themeModeProvider.notifier).change,
+          child: Icon(themeMode.icon, color: context.themeColor.mediumEmphasis),
+        );
+      }),
     );
   }
 }
