fc9948efb938cbf189b4a224155fedccf2ea966a
おりょう <oryo.developer@gmail.com>
2024-06-07 19:34:57 +0900

    ArticlesPageAppBarBottomを作成

--- lib/ui/pages/articles_page/widgets/articles_page_app_bar/articles_page_app_bar.dart
+++ lib/ui/pages/articles_page/widgets/articles_page_app_bar/articles_page_app_bar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_qiita_app/ui/pages/articles_page/widgets/articles_page_app_bar/articles_page_search_text_field.dart';
+import 'package:flutter_qiita_app/ui/pages/articles_page/widgets/articles_page_app_bar/articles_page_app_bar_bottom.dart';
 import 'package:flutter_qiita_app/ui/widgets/change_theme_mode_button.dart';
 import 'package:flutter_qiita_app/ui/widgets/logo.dart';
 
@@ -17,8 +17,7 @@ class ArticlesPageAppBar extends StatelessWidget
     return AppBar(
       title: const Logo(),
       actions: const [ChangeThemeModeButton(), SizedBox(width: 16)],
-      bottom: const ArticlesPageSearchTextField(),
-      centerTitle: true,
+      bottom: const ArticlesPageAppBarBottom(),
     );
   }
 }
---
+++ lib/ui/pages/articles_page/widgets/articles_page_app_bar/articles_page_app_bar_bottom.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_qiita_app/extensions/listenable_extension.dart';
+import 'package:flutter_qiita_app/ui/pages/articles_page/articles_page_provider.dart';
+import 'package:flutter_qiita_app/ui/widgets/qiita_text_field.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class ArticlesPageAppBarBottom extends HookConsumerWidget
+    implements PreferredSizeWidget {
+  const ArticlesPageAppBarBottom({super.key});
+
+  @override
+  Size get preferredSize {
+    return const Size.fromHeight(kToolbarHeight);
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final (focusNode, hasPrimaryFocus) = useFocusNode().listenableSelector(
+      (focusNode) {
+        return focusNode.hasPrimaryFocus;
+      },
+    );
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
+      child: Row(children: [
+        Expanded(
+          child: QiitaTextField(
+            focusNode: focusNode,
+            textInputAction: TextInputAction.search,
+            onSubmitted: (query) async {
+              await ref
+                  .read(articlesPageProvider.notifier)
+                  .fetchFirstPageArticles(query: query);
+            },
+            hintText: '記事、質問を検索',
+            prefixIcon: const Icon(Icons.search),
+          ),
+        ),
+        if (hasPrimaryFocus)
+          Padding(
+            padding: const EdgeInsets.only(left: 16),
+            child: GestureDetector(
+              onTap: focusNode.unfocus,
+              child: const Text(
+                'キャンセル',
+                style: TextStyle(
+                  fontSize: 16,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+          ),
+      ]),
+    );
+  }
+}
