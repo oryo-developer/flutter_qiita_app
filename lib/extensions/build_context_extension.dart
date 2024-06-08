3169e786593bfb85f8063bdacbe39f37619750cd
おりょう <oryo.developer@gmail.com>
2024-06-08 17:15:19 +0900

    最小のpaddingを設定できるようにした

--- lib/extensions/build_context_extension.dart
+++ lib/extensions/build_context_extension.dart
@@ -2,18 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter_qiita_app/ui/theme/theme_color/theme_color.dart';
 
 extension BuildContextExtension on BuildContext {
-  EdgeInsets get padding {
-    return MediaQuery.paddingOf(this);
+  ThemeColor get themeColor {
+    final brightness = Theme.of(this).brightness;
+    return ThemeColor(brightness: brightness);
+  }
+
+  EdgeInsets padding({
+    double? minLeft,
+    double? minTop,
+    double? minRight,
+    double? minBottom,
+  }) {
+    return MediaQuery.paddingOf(this).copyWith(
+      left: minLeft,
+      top: minTop,
+      right: minRight,
+      bottom: minBottom,
+    );
   }
 
   Future<T?> push<T>({required Widget widget}) {
     return Navigator.push(this, MaterialPageRoute(builder: (context) {
       return widget;
     }));
-  }
-
-  ThemeColor get themeColor {
-    final brightness = Theme.of(this).brightness;
-    return ThemeColor(brightness: brightness);
   }
 }
