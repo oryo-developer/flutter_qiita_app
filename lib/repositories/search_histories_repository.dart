e8cd6e30aa6f0c0cb6b44d93e349e63714a7fed3
おりょう <oryo.developer@gmail.com>
2024-06-09 17:24:41 +0900

    SearchHistoriesRepositoryを作成

---
+++ lib/repositories/search_histories_repository.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_qiita_app/shared_preferences.dart';
+
+class SearchHistoriesRepository {
+  final _key = 'SEARCH_HISTORIES';
+
+  List<String>? get() {
+    return sharedPreferences.getStringList(_key);
+  }
+
+  Future<bool> set({required List<String> searchHistories}) {
+    return sharedPreferences.setStringList(_key, searchHistories);
+  }
+}
