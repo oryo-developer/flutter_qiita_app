e297c60601e5515879e32d2068f9843b1cb6d201
おりょう <oryo.developer@gmail.com>
2024-06-10 11:43:53 +0900

    リファクタリング

--- lib/providers/pages/articles_page_provider.dart
+++ lib/providers/pages/articles_page_provider.dart
@@ -1,14 +1,11 @@
-import 'dart:math';
-
-import 'package:flutter_qiita_app/main.dart';
 import 'package:flutter_qiita_app/models/article/article.dart';
-import 'package:flutter_qiita_app/repositories/article_repository/article_repository.dart';
+import 'package:flutter_qiita_app/services/article_service.dart';
 import 'package:flutter_qiita_app/states/pages/articles_page_state/articles_page_state.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final articlesPageProvider =
     StateNotifierProvider<ArticlesPageNotifier, ArticlesPageState>((ref) {
-  return throw UnimplementedError();
+  throw UnimplementedError();
 });
 
 class ArticlesPageNotifier extends StateNotifier<ArticlesPageState> {
@@ -18,32 +15,29 @@ class ArticlesPageNotifier extends StateNotifier<ArticlesPageState> {
     fetchFirstPageArticles();
   }
 
-  final _articleRepository = ArticleRepository(dio);
+  final _articleService = ArticleService();
 
-  Future<List<Article>> _fetchArticles({required int page}) async {
-    final response = await _articleRepository.fetchArticles(
-      page: page,
-      query: state.query,
-    );
-    final headers = response.response.headers;
-    final totalCount = int.parse(headers.value('Total-Count') ?? '0');
-    final maxPage = min(100, (totalCount / 20).ceil());
-    state = state.copyWith(page: page + 1, maxPage: maxPage);
-    return response.data;
-  }
-
-  Future<void> fetchFirstPageArticles({String? query}) async {
+  Future<void> fetchFirstPageArticles() async {
     final articles = await _fetchArticles(page: 1);
     state = state.copyWith(articles: articles);
   }
 
   Future<void> fetchNextPageArticles() async {
-    if (state.isNextPageArticlesFetching || state.maxPage! < state.page) return;
-    state = state.copyWith(isNextPageArticlesFetching: true);
+    if (state.isFetchingNextPageArticles || state.maxPage! < state.page) return;
+    state = state.copyWith(isFetchingNextPageArticles: true);
     final articles = await _fetchArticles(page: state.page);
     state = state.copyWith(
-      isNextPageArticlesFetching: false,
+      isFetchingNextPageArticles: false,
       articles: state.articles! + articles,
     );
+  }
+
+  Future<List<Article>> _fetchArticles({required int page}) async {
+    final (
+      maxPage,
+      articles,
+    ) = await _articleService.fetchArticles(page: page, query: state.query);
+    state = state.copyWith(page: page + 1, maxPage: maxPage);
+    return articles;
   }
 }
