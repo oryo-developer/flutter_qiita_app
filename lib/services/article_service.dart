648c466511b55f2e8ef90f408ffc3b23ec2aad92
おりょう <oryo.developer@gmail.com>
2024-06-10 11:43:29 +0900

    ArticleServiceを作成

---
+++ lib/services/article_service.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_qiita_app/extensions/http_response_extension.dart';
+import 'package:flutter_qiita_app/main.dart';
+import 'package:flutter_qiita_app/models/article/article.dart';
+import 'package:flutter_qiita_app/repositories/article_repository/article_repository.dart';
+
+class ArticleService {
+  final _articleRepository = ArticleRepository(dio);
+
+  Future<(int, List<Article>)> fetchArticles({
+    required int page,
+    String? query,
+  }) async {
+    final httpResponse = await _articleRepository.fetchArticles(
+      page: page,
+      query: query,
+    );
+    return (httpResponse.maxPage, httpResponse.data);
+  }
+}
